1445d27a99a1b22cef53b8246bee6883833da3dd
jellz <danielgulic@gmail.com>
2019-03-16 23:20:10 +1100

    finished for now

---
+++ assets/repos.js
@@ -0,0 +1,25 @@
+const USERNAME_REGEX = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+const INACTIVE_MARK = 1.814e+9; // 3 weeks in ms - after a project is inactive for 3 weeks or longer, it is considered abandoned
+
+$('#main-form').on('submit', async (e) => {
+  e.preventDefault();
+  let username = $('#gh-username-input').val().trim();
+  if (!USERNAME_REGEX.test(username)) return alert('Invalid username');
+  let res = await fetch(`https://api.github.com/users/${username}/repos`);
+  let json = await res.json();
+  let repos = json.sort((a, b) => new Date(a.pushed_at).getTime() - new Date(b.pushed_at).getTime());
+  $('.list').empty();
+  initRepoList(repos);
+});
+
+var initRepoList = (repos) => {
+  repos.forEach((r) => {
+    r.pushed_at_humanized = timeAgo(new Date(r.pushed_at));
+  });
+  let options = {
+    valueNames: [ 'name', 'pushed_at_humanized' ],
+    item: '<li><h3 class="name"></h3> <p class="pushed_at_humanized"></p></li>'
+  };
+  console.log(repos);
+  let repoList = new List('repo-list-wrapper', options, repos);
+}
